1e774aa7590293d3325d3e3597330173b768c9ce
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-18 18:30:22 +0200

    more lessons added

---
+++ stringreplacement.py
@@ -0,0 +1,9 @@
+age = 24
+print("my age is " + str(age) + " years")
+
+print("my age is {0} years".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31, "jan", "mar", "may", "jul", "aug", "oct", "dec"))
+
+print("there are {0} days in Jan, mar, may, jul, aug, oct, dec".format(31))
