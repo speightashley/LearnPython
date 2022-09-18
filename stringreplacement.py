470309c9f5f3f486abaf89d4aeeecdd3eae51453
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-18 18:32:39 +0200

    Changed git editor Vim instead of nano

--- stringreplacement.py
+++ stringreplacement.py
@@ -7,3 +7,5 @@ print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
       .format(31, "jan", "mar", "may", "jul", "aug", "oct", "dec"))
 
 print("there are {0} days in Jan, mar, may, jul, aug, oct, dec".format(31))
+
+# new line added for testing
