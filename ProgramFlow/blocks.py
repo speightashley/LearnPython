1f37b02be123417dfb35e2d612c8cf2b54a3b429
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-19 19:08:09 +0200

    if and else

--- ProgramFlow/blocks.py
+++ ProgramFlow/blocks.py
@@ -6,4 +6,9 @@ name = input("Please enter you name: ")
 age = int(input("How old are you, {0}? ".format(name)))
 print(age)
 
+if age >= 18:
+    print("You are old enough to vote")
+    print("Please put an x in the box")
+else:
+    print("Please come back in {} years".format(18 - age))
 
