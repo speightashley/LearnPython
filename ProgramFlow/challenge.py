6d1af21478a46bd84585f791866b96d52a6b09dc
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-20 16:23:34 +0200

    challenge

---
+++ ProgramFlow/challenge.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+if age < 18:
+    print("You're too young")
+elif age > 31:
+    print("You're too old innit?")
+else:
+    print("You can go on holiday on an 18 to 30 holiday")
