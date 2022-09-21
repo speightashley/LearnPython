41502ac8c2d9a78b0a93936986ddfb3650aab9ad
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-21 19:11:16 +0200

    more loops and lists

--- ProgramFlow/conditions.py
+++ ProgramFlow/conditions.py
@@ -1,6 +1,6 @@
 age = int(input("How old are you? "))
 
-if 16 <= age <= 65:
+if 16 <= age <= 65:  # if age in range(16, 66):
     print("Have a good day at work")
 
 if age < 16 or age > 65:
---
+++ loops/ranges.py
@@ -0,0 +1,5 @@
+for i in range(1, 20, 2):
+    print("I is now {}".format(i))
+
+for i in range(20, 0, -2): # negative steps
+    print("I is now {}".format(i))
