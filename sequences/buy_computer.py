f58f118b0b66dbaf5c5cbe60d61b856549c76a57
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-26 20:22:57 +0200

    menus

--- sequences/buy_computer.py
+++ sequences/buy_computer.py
@@ -8,21 +8,14 @@ available_parts = ["computer",
 current_choice = "-"
 computer_parts = []  # create and empty list
 
+valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append("Computer")
-        elif current_choice == '2':
-            computer_parts.append("Monitor")
-        elif current_choice == '3':
-            computer_parts.append("Keyboard")
-        elif current_choice == '4':
-            computer_parts.append("mouse")
-        elif current_choice == '5':
-            computer_parts.append("Mouse Mat")
-        elif current_choice == '6':
-            computer_parts.append("HDMI Cable")
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("Please add options form the list below")
         for number, part in enumerate(available_parts):
