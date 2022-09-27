746888a62e78f91b952d8f850cb06ca1169b97fc
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-27 20:37:56 +0200

    lists

--- sequences/buy_computer.py
+++ sequences/buy_computer.py
@@ -12,10 +12,17 @@ valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
 
 while current_choice != '0':
     if current_choice in valid_choices:
-        print("Adding {}".format(current_choice))
+
         index = int(current_choice) - 1
         chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
+        if chosen_part in computer_parts:
+            # if it's already in, so remove it
+            print("Removing {}".format(current_choice))
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choice))
+            computer_parts.append(chosen_part)
+        print("Your list now contains: {}".format(computer_parts))
     else:
         print("Please add options form the list below")
         for number, part in enumerate(available_parts):
