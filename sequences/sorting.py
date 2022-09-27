c95f022047bb236d1a7eb43e6a7f080e12676559
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-27 20:56:36 +0200

    sorting

---
+++ sequences/sorting.py
@@ -0,0 +1,9 @@
+pangram = "The quick brown fox jumps over the lazy dog"
+
+letter = sorted(pangram)
+
+print(letter)
+
+numbers = [2.4, 5.6, 5.7, 7.8, 2.6, 1.6]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
