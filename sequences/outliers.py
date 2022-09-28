aee1cc5d9fb16797632507e7a5f2a5408914338d
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-28 21:44:33 +0200

    More stuff about lists

--- sequences/outliers.py
+++ sequences/outliers.py
@@ -1,6 +1,5 @@
 data = [4, 5, 104, 105, 110, 120, 130, 150, 170]
 
-
 print(data)
 del data[14:]
 print(data)
@@ -21,4 +20,20 @@ for index, value in enumerate(data):
         break
 
 del data[:stop]
+print(data)
+
+# process high values of the list
+
+start = 0
+
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_value:
+        # We have the index of the last item to keep.
+        # set start t teh position of the first
+        # item to delete starts at index + 1
+        start = index + 1
+        break
+
+del data[start:]
+
 print(data)
