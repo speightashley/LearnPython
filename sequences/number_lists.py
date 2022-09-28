fef251f81fa5101a52ef41dde9619a3fb12b0ee0
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-28 18:15:37 +0200

    sorting

--- sequences/number_lists.py
+++ sequences/number_lists.py
@@ -1,5 +1,12 @@
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
+empty_list = []
+
+numbers = even + odd
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
 
 print(min(even))
 print(max(even))
--- sequences/sorting.py
+++ sequences/sorting.py
@@ -7,3 +7,15 @@ print(letter)
 numbers = [2.4, 5.6, 5.7, 7.8, 2.6, 1.6]
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
+
+missing_letter = sorted("The quick brown fox jumped over the lazy dog", key=str.casefold)
+
+names = ["Graham",
+         "jon",
+         "Terry",
+         "eric",
+         "terry"
+         ]
+
+names.sort(key=str.casefold)
+
