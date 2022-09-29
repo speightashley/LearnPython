d4c600284d14a75526b660192f4df60ebcec2432
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-29 21:17:00 +0200

    nested lists

--- sequences/number_lists.py
+++ sequences/number_lists.py
@@ -2,29 +2,33 @@ even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 empty_list = []
 
-numbers = even + odd
+numbers = [even, odd]
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 print(numbers)
 
+for number_list in numbers:
+    print(number_list)
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
-
-print()
-print(len(even))
-print(len(odd))
-
-print()
-print("mississippi".count("iss"))
-
-even.extend(odd)
-print(id(even))
-print(even)
-another_even = even
-print(another_even)
-even.sort(reverse=True)
-print(id(even))
-print(even)
+    for value in number_list:
+        print(value)
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+#
+# print()
+# print(len(even))
+# print(len(odd))
+#
+# print()
+# print("mississippi".count("iss"))
+#
+# even.extend(odd)
+# print(id(even))
+# print(even)
+# another_even = even
+# print(another_even)
+# even.sort(reverse=True)
+# print(id(even))
+# print(even)
