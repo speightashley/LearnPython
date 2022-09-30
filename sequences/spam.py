b0fce8c918720519b5dbf9d9700636200e6fdc8b
Ashley Speight <ashleyspeight86@gmail.com>
2022-09-30 20:35:47 +0200

    spam

--- sequences/spam.py
+++ sequences/spam.py
@@ -8,3 +8,12 @@ menu = [
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
     ["spam", "egg", "spam", "spam", "bacon", "spam"],
 ]
+
+for meal in menu:
+    if "spam" not in meal:
+        print(meal)
+
+        for item in meal:
+            print(item)
+    else:
+        print("{0} has a spam score of {1}".format(meal, meal.count("spam")))
