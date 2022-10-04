897a3031b426a71333b9f38412f029b327d9068e
Ashley Speight <ashleyspeight86@gmail.com>
2022-10-04 20:47:45 +0200

    more tuple stuff

--- sequences/tuplesintro.py
+++ sequences/tuplesintro.py
@@ -7,10 +7,22 @@ budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the lightning", "metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
+
+table = ("Coffee table", 200, 100, 75, 34.50)
+print(table[1] * table[2])
+
+name, length, width, height, price = table
+
+print(length * width)
 
 metallica2 = list(metallica)
 
